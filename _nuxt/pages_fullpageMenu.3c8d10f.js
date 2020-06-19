a00f9fac671d47a563ff1e837598b2e53748c2bb
Patrick Madlang-awa <madlangawapat22@gmail.com>
2020-06-19 09:22:20 +0800

    fixed laptop resolution

---
+++ _nuxt/pages_fullpageMenu.3c8d10f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{178:function(e,n,t){"use strict";t.r(n);var c={},r=t(16),component=Object(r.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,"4b308fcf",null);n.default=component.exports}}]);
